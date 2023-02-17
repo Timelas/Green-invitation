10caeb7bdfdb693bc3690ac5ba84700675e18341
Timelas <lom.a.s@icloud.com>
2023-02-17 14:31:15 +0300

    new-format fix

--- src/components/InfoDate/InfoDate.js
+++ src/components/InfoDate/InfoDate.js
@@ -8,10 +8,10 @@ function InfoDate() {
         <p className="info-date__who">Дорогие</p>
         <p className="info-date__who">родные и друзья!</p>
         <p className="info-date__text">Один день в этом году будет для нас особенным и мы хотим провести его в кругу близких и друзей. С большим удовольствием приглашаем вас на знаменательный праздник - </p>
-        <div className="info-date__our-wedding" id="place"><p className="info-date__our-wedding-text">Нашу свадьбу!</p></div>
+        <div className="info-date__our-wedding"><p className="info-date__our-wedding-text">Нашу свадьбу!</p></div>
       </div>
       <div className="info-date__stone"></div>
-      <div className="info-date__place">
+      <div className="info-date__place" id="place">
         <div className="info-date__check-in">
           <p className="info-date__what">Торжественная регистрация</p>
           <p className="info-date__where">Дворец Бракосочетаяния 1 <br /> Английская набережная, 28 <br /> в 15.30</p>
